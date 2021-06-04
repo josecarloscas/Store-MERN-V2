e0c4aacad7f15763d0160f32143da0456cebc71e
Jose Castro <42446919+josecarloscas@users.noreply.github.com>
2021-06-03 22:44:18 -0500

    Update connect.js

--- models/connect.js
+++ models/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose
   .connect(
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/shopping_cart',
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/storedb',
     { useNewUrlParser: true, useCreateIndex: true }
   )
   .then(() => console.log('Connected to the DB.'))
