b435b8a9da0f61b1064c6c016726ced9f14cb903
Jose Castro <42446919+josecarloscas@users.noreply.github.com>
2021-06-09 16:42:55 -0500

    Update Item.js

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+//Se define el componente Item que permite verificar si el usuario logeado tiene productos en el carro 
+//Tambien permite el agregar un producto al carro y la vista del boton (✔) cuando este fue agregado
 class Item extends Component {
   _isMounted = false;
 
   state = {
     isInCart: false,
   };
-
+  //verificar si el usuario tiene productos
   componentDidMount() {
     this._isMounted = true;
 
@@ -18,7 +19,7 @@ class Item extends Component {
   componentWillUnmount() {
     this._isMounted = false;
   }
-
+  //Si tiene productos en el carro estos productos tendrán un estado que refleja que están en el carro (isInCart)
   userHasItem = itemId => {
     if (localStorage.getItem('token')) {
       fetch(`/user/items/${itemId}`, {
@@ -38,7 +39,7 @@ class Item extends Component {
         });
     }
   };
-
+  //Añadir productos al carrito 
   addItemToCart = async () => {
     if (!localStorage.getItem('token')) {
       this.props.history.push('/login', {
@@ -59,7 +60,7 @@ class Item extends Component {
       }
     }
   };
-
+  //Mensaje de alerta 
   showAlertMessage = message => {
     const alertDiv = document.getElementById('addConfirmAlert');
     const messageNode = document.createTextNode(message);
@@ -71,7 +72,7 @@ class Item extends Component {
       alertDiv.classList.replace('d-none', 'd-block');
     }
   };
-
+  //Llamado al metodo post para un posterior guardado de los productos del carro en una lista 
   postData = async (url, data = {}) => {
     const response = await fetch(url, {
       method: 'POST',
@@ -84,7 +85,7 @@ class Item extends Component {
     return await response.json();
   };
 
-
+  //Renderizamos la vista general de los productos en el inicio y los botones para agregar los productos al carro 
   render() {
     const { name, description, price, imageUrl } = this.props;
     return (
