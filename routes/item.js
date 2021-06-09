a3a47417101c2fcf7b10007026b99805ec41d5f3
Jose Castro <42446919+josecarloscas@users.noreply.github.com>
2021-06-09 17:37:51 -0500

    Update item.js

--- routes/item.js
+++ routes/item.js
@@ -3,6 +3,7 @@ const validators = require('../handlers/validators');
 const controller = require('../controllers/itemdbController');
 const { loggedIn } = require('../middlewares/auth');
 
+//Se definen las rutas que permiten el manejo de los productos en la base de datos 
 const router = express.Router();
 
 router.post('/additemdb', validators.itemDbValidator, controller.addItemdb);
