bd288f6e2fcc5e3dafa291930510ed6f54916527
Jose Castro <42446919+josecarloscas@users.noreply.github.com>
2021-06-09 17:12:42 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//Espaciado de columnas y estilo 
 import './css/App.css';
+//Cabecera
 import Header from './components/layout/Header';
+//Lista de priductos
 import ItemsList from './components/ItemsList';
+//Componente con el formulario de inicio de sesión
 import LoginForm from './components/LoginForm';
+//Componente con el mensaje de error 404
 import NotFound from './components/layout/NotFound';
+//Componente con el formulario de registro
 import RegisterForm from './components/RegisterForm';
+//Componente con la vista del carro
 import Cart from './components/Cart';
+//Componente con la vista del pie de pagina con el enlace el proyecto
 import Footer from './components/layout/Footer';
 
+//Se define la estructura general de la vista de la aplicación 
 function App() {
   return (
     <div>
