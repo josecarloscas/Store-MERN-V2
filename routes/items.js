c7991cce9b788e00efa6c407189d9ad1aec91ec4
Jose Castro <42446919+josecarloscas@users.noreply.github.com>
2021-06-09 17:38:16 -0500

    Update items.js

--- routes/items.js
+++ routes/items.js
@@ -3,6 +3,7 @@ const validators = require('../handlers/validators');
 const controller = require('../controllers/itemController');
 const { loggedIn } = require('../middlewares/auth');
 
+//Se definen las rutas que permiten el manejo de los productos en el carro 
 const router = express.Router();
 
 router.get('/', controller.getAllItems);
