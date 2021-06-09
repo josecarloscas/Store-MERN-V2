ca455176b618111c16c0815cc93b8c4f10163c6e
Jose Castro <42446919+josecarloscas@users.noreply.github.com>
2021-06-09 17:31:48 -0500

    Update getDetailedItems.js

--- helpers/getDetailedItems.js
+++ helpers/getDetailedItems.js
@@ -1,5 +1,6 @@
 const Item = require('../models/Item');
-
+// Se define la logica que permite el calculo total de la compra dependiento de la cantidad de los productos del carro,
+// Siendo esta cantidad la que define el precio por cada producto en la lista 
 module.exports = async items => {
   let detailedItems = [];
   let totalPrice = 0;
