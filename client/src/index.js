dbc8d44b4e3daf234555cc0ded0c50fe3bed848d
Jose Castro <42446919+josecarloscas@users.noreply.github.com>
2021-06-09 17:14:07 -0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+//Importamos los iconos usados en este proyecto
 import {
   faShoppingCart,
   faStore,
