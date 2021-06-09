ccd52cc128818016f58c5ea52d8fb0d35b2b89da
Jose Castro <42446919+josecarloscas@users.noreply.github.com>
2021-06-09 17:38:41 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const validators = require('../handlers/validators');
 const controller = require('../controllers/userController');
 const { loggedIn } = require('../middlewares/auth');
 
+//Se definen las rutas que permiten el manejo de los usuarios en la base de datos 
 const router = express.Router();
 
 router.post('/signup', validators.signUpValidator, controller.signUp);
